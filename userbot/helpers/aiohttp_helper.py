e672dfb6765af7224c5629b394fb1a9352dad04a
Ralls <aliza6746@gmail.com>
2021-10-20 19:39:45 +0300

    Add files via upload

---
+++ userbot/helpers/aiohttp_helper.py
@@ -0,0 +1,24 @@
+# Ralls SOURCE
+# ARABIC USERBOT - BY: @N7QQQ
+
+import aiohttp
+
+
+class AioHttp:
+    @staticmethod
+    async def get_json(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.json()
+
+    @staticmethod
+    async def get_text(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.text()
+
+    @staticmethod
+    async def get_raw(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.read()
